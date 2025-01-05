c570eda3ec59d110efbf4b0171b8d23fae9a41a3
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-01-05 22:46:11 +0530

    Added styling.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div className="w-[350px] h-[50px] bg-gray-700 rounded-[13px]">
         <div className="flex-row items-center gap-2 p-10">
-          <h1>Move</h1>
+          <h1 className='flex-row'>Move</h1>
           <h1>frame</h1>
           <h1>code</h1>
           <h1>text</h1>
