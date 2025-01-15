2a6a4cb5b1c826ff9ea9de244868246b0115616d
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-01-15 22:11:53 +0530

    Added other images &  fixed styling.

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,24 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div className="w-[350px] h-[50px] bg-gray-700 rounded-[13px]">
-        <div className={styles.img}>
-          <Image src="/images/move.svg" alt='' fill className={styles.imgType} />
+        <div className="imgConatiner">
+          <div className={styles.img}>
+            <Image src="/images/move.svg" alt='' fill />
+          </div>
+
+          <div className={styles.img}>
+            <Image src="/images/frame.svg" alt='' fill />
+          </div>
+
+          <div className={styles.img}>
+            <Image src="/images/rectangle.svg" alt='' fill />
+          </div>
+
+          <div className={styles.img}>
+          <Image src="/images/pen.svg" alt='' fill />
         </div>
+        </div>
+        
        </div>
 
     </main>
