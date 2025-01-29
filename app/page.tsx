a8f1f488d1a67a418ed6539aa859355a1261ef27
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-01-29 15:35:47 +0530

    Fixed the images bug, Fixed the styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 import Image from 'next/image';
-import styles from "./home.module.css";
+import styles from './home.module.css';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div className="w-[350px] h-[50px] bg-gray-700 rounded-[13px]">
-        <div className="imgContainer">
+        <div className="flex flex-row  gap-10 pt-2 pl-1 align-center justify-center filter invert transition-all duration-200">
           <div className={styles.img}>
-            <Image src="/images/move.svg" alt='' fill />
+            <Image src="/images/move.svg" alt=''  width={20}  height={20}/>
           </div>
 
           <div className={styles.img}>
-            <Image src="/images/frame.svg" alt='' fill />
+            <Image src="/images/frame.svg" alt='' width={20} height={20}/>
           </div>
 
           <div className={styles.img}>
-            <Image src="/images/rectangle.svg" alt='' fill />
+            <Image src="/images/rectangle.svg" alt='' width={20} height={20}/>
           </div>
 
           <div className={styles.img}>
-          <Image src="/images/pen.svg" alt='' fill />
+          <Image src="/images/pen.svg" alt='' width={20} height={20} />
         </div>
         </div>
         
